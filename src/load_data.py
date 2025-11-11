02e74396dbe8eb9e8744d6effb03b108e1340c6a
Daragh Hollman <hollmandaragh@gmail.com>
2025-11-11 10:08:32 +0000

    Abstracted loading data code to separate function, added training test split, use test set for create_model.py

---
+++ src/load_data.py
@@ -0,0 +1,78 @@
+"""Function to load reduced data and perform train/test split"""
+
+import pandas as pd
+
+
+def load_reduced_data():
+
+    SEED = 1785
+
+    # Load features datasets
+    # These are in 4 different data sets which we need to combine
+    inputs = {
+        "Solar Wind": "./data/reduced/solar_wind_samples.csv",
+        "BS Magnetosheath": "./data/reduced/bs_magnetosheath_samples.csv",
+        "MP Magnetosheath": "./data/reduced/mp_magnetosheath_samples.csv",
+        "Magnetopause": "./data/reduced/magnetosphere_samples.csv",
+    }
+
+    all_samples = []
+    for region_type in inputs.keys():
+        all_samples.append(pd.read_csv(inputs[region_type]))
+
+    # Combine into one dataframe
+    all_samples = pd.concat(all_samples, ignore_index=True).dropna()
+
+    # Combine labelled for bow shock and magnetopause adjacent magnetosheath
+    # samples, and balance classes
+    all_samples["Label"] = all_samples["Label"].replace(
+        "BS Magnetosheath", "Magnetosheath"
+    )
+    all_samples["Label"] = all_samples["Label"].replace(
+        "MP Magnetosheath", "Magnetosheath"
+    )
+
+    # Check if there are extreme values in the dataset and remove them as these
+    # are either an ICME, or non-physical, neither of which we want to include
+    # in our training.
+    extreme_rows = all_samples.loc[
+        (all_samples["Mean |B|"] >= 5000)
+        | (all_samples["Standard Deviation |B|"] >= 5000)
+    ]
+    all_samples = all_samples.drop(extreme_rows.index)
+
+    # Balance classes through undersampling
+    smallest_class_length = all_samples["Label"].value_counts().min()
+
+    all_samples = all_samples.groupby("Label").apply(
+        lambda class_data: class_data.sample(n=smallest_class_length, random_state=SEED)
+    )
+
+    # Print a summary
+    print(
+        f"""
+        Full dataset lengths:
+        Size: {len(all_samples)}
+            SW: {len(all_samples.loc[all_samples["Label"] == "Solar Wind"])}
+            MSh: {len(all_samples.loc[all_samples["Label"] == "Magnetosheath"])}
+            MSp: {len(all_samples.loc[all_samples["Label"] == "Magnetosphere"])}
+        """
+    )
+
+    # We want to create a train/test split so we can quantify performance of
+    # the final model after optimisation. OOB Score is used to validate the
+    # training. We use a fixed seed to ensure this split is the same across all
+    # functions.
+    test_fraction = 0.1
+    training_data = all_samples.sample(frac=1 - test_fraction, random_state=SEED)
+    test_data = all_samples.drop(index=training_data.index)
+
+    print(
+        f"""
+        Data split into train/test split with test fraction = {test_fraction}
+        Size of training data = {len(training_data)}
+        Size of test data = {len(test_data)}
+        """
+    )
+
+    return training_data, test_data
--- src/optimise_model.py
+++ src/optimise_model.py
@@ -10,6 +10,8 @@ import optuna
 import pandas as pd
 import sklearn.ensemble
 
+from load_data import load_reduced_data
+
 SEED = 1785
 n_cores = max(multiprocessing.cpu_count() - 1, 1)
 
@@ -26,57 +28,7 @@ def main():
     #                          LOADING & VALIDATING DATA                                 #
     ######################################################################################
 
-    # Load features datasets
-    # These are in 4 different data sets which we need to combine
-    inputs = {
-        "Solar Wind": "./data/reduced/solar_wind_samples.csv",
-        "BS Magnetosheath": "./data/reduced/bs_magnetosheath_samples.csv",
-        "MP Magnetosheath": "./data/reduced/mp_magnetosheath_samples.csv",
-        "Magnetopause": "./data/reduced/magnetosphere_samples.csv",
-    }
-
-    all_samples = []
-    for region_type in inputs.keys():
-        all_samples.append(pd.read_csv(inputs[region_type]))
-
-    # Combine into one dataframe
-    all_samples = pd.concat(all_samples, ignore_index=True).dropna()
-
-    # Combine labelled for bow shock and magnetopause adjacent magnetosheath
-    # samples, and balance classes
-    all_samples["Label"] = all_samples["Label"].replace(
-        "BS Magnetosheath", "Magnetosheath"
-    )
-    all_samples["Label"] = all_samples["Label"].replace(
-        "MP Magnetosheath", "Magnetosheath"
-    )
-
-    # Check if there are extreme values in the dataset and remove them as these
-    # are either an ICME, or non-physical, neither of which we want to include
-    # in our training.
-    extreme_rows = all_samples.loc[
-        (all_samples["Mean |B|"] >= 5000)
-        | (all_samples["Standard Deviation |B|"] >= 5000)
-    ]
-    all_samples = all_samples.drop(extreme_rows.index)
-
-    # Balance classes through undersampling
-    smallest_class_length = all_samples["Label"].value_counts().min()
-
-    all_samples = all_samples.groupby("Label").apply(
-        lambda class_data: class_data.sample(n=smallest_class_length, random_state=SEED)
-    )
-
-    # Print a summary
-    print(
-        f"""
-        Full dataset lengths:
-        Size: {len(all_samples)}
-            SW: {len(all_samples.loc[all_samples["Label"] == "Solar Wind"])}
-            MSh: {len(all_samples.loc[all_samples["Label"] == "Magnetosheath"])}
-            MSp: {len(all_samples.loc[all_samples["Label"] == "Magnetosphere"])}
-        """
-    )
+    training_data, _ = load_reduced_data()
 
     ######################################################################################
     #                                   OPTIMISING MODEL                                 #
@@ -90,7 +42,7 @@ def main():
         for line in f:
             features.append(line.strip())
 
-    training_data = all_samples[features + ["Label"]]
+    training_data = training_data[features + ["Label"]]
     training_x = training_data.drop(columns="Label")
     training_y = training_data["Label"]
 
