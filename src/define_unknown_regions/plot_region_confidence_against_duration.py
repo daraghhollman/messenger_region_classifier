4fc258568321d1d3ab5ce4df9c7e36fc789c64a7
Daragh Hollman <hollmandaragh@gmail.com>
2025-11-24 15:05:23 +0000

    Removed split by region

--- src/define_unknown_regions/plot_region_confidence_against_duration.py
+++ src/define_unknown_regions/plot_region_confidence_against_duration.py
@@ -41,14 +41,6 @@ for selection in fit.keys():
     else:
         regions_subset = regions.loc[regions["Label"] == selection]
 
-        # ax.scatter(
-        #     regions_subset["Duration"],
-        #     regions_subset["Confidence"],
-        #     marker=".",
-        #     color=fit[selection]["Colour"],
-        #     zorder=5,
-        # )
-
         pars, cov = scipy.optimize.curve_fit(
             fit_function, regions_subset["Duration"], regions_subset["Confidence"]
         )
@@ -71,11 +63,14 @@ fig.colorbar(hist, ax=ax, label="# Regions")
 x_range = np.linspace(1, regions["Duration"].max(), 1000)
 for key, values in fit.items():
 
+    if key != "All":
+        continue
+
     ax.plot(
         x_range,
         fit_function(x_range, *values["Params"]),
         color=values["Colour"],
-        lw=5 if key == "All" else 2,
+        lw=2,
         label=f"Least squares fit (Region: {key})",
     )
 
@@ -111,12 +106,3 @@ ax.margins(0)
 ax.legend()
 
 fig.savefig("./figures/region_confidence_vs_duration.pdf", format="pdf")
-
-# We also need to save the parameters so we can define unknown regions.
-pd.DataFrame(
-    {
-        "Parameter": ["A", "B"],
-        "Value": fit["All"]["Params"],
-        "Error": fit["All"]["Errors"],
-    }
-).to_csv("./data/postprocessing/unknown_region_parameters.csv")
